bd2a8e8a73469ab54900f73c7a0412896b65c229
mzazimhenga22 <waitathukaranja@gmail.com>
2025-06-04 05:37:31 -0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,14 +23,15 @@ android {
     }
 
 compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
     isCoreLibraryDesugaringEnabled = true
 }
 
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+kotlinOptions {
+    jvmTarget = "17"
+}
+
 
     buildTypes {
         getByName("release") {
